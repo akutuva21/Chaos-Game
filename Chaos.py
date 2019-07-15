e0dbbe5b9d650f63f311da79cca5a925573d3efb
akutuva21 <44119804+akutuva21@users.noreply.github.com>
2019-07-15 19:39:35 -0400

    Optimized a Few Functions
    Added a Rotation Function
    Function to generate initial points

--- Chaos.py
+++ Chaos.py
@@ -1,56 +1,55 @@
 import matplotlib.pyplot as plt
 import math, random
 
-L = 5
 t = ''
 s = ''
 p = ''
-num = 0
-
-#a = (0, math.sqrt(3)*L/4)
-#b = (-L/2, -math.sqrt(3)*L/4)
-#c = (L/2, -math.sqrt(3)*L/4)
-
-x = (0,0)
+i = 0
+a, b, c, d, e = (0,0), (0,0), (0,0), (0,0), (0,0)
 
 xvals = []
 yvals = []
 
-N = int(input("Enter the number of iterations: "))
+numiter = int(input("Enter the number of iterations: "))
 shape = str(input("Enter what shape you desire (t for triangle, s for square, p for pentagon): "))
 while True:
     if shape == 't' or shape == 's' or shape == 'p':
         break
     shape = input("Sorry, wrong input. Try again: ")
-i = 0
+rotation = int(input("Enter the degrees of rotation: "))
+while True:
+    if 0 <= rotation <= 360:
+        break
+    shape = int(input("Sorry, invalid output. Try again: "))
 
-if shape == 't':
-    num = 3
-    a = (0, math.sqrt(3) * L / 4)
-    b = (-L / 2, -math.sqrt(3) * L / 4)
-    c = (L / 2, -math.sqrt(3) * L / 4)
-if shape == 's':
-    num = 4
-    a = (-L, L)
-    b = (L, L)
-    c = (L, -L)
-    d = (-L, -L)
-if shape == 'p': #http://mathworld.wolfram.com/RegularPentagon.html
-    num = 5
-    a = (0, 1)
-    b = (math.sin(2* math.pi/5), math.cos(2 * math.pi/5))
-    c = (math.sin(4* math.pi/5), -math.cos(math.pi/5))
-    d = (-math.sin(4* math.pi/5), -math.cos(math.pi/5))
-    e = (-math.sin(2*math.pi/5), math.cos(2*math.pi/5))
+rotation *= math.pi/180 #https://en.wikipedia.org/wiki/Rotation_of_axes
+
+def pointfinder(N,n):
+    x = math.cos(2*math.pi*n/N)
+    y = math.sin(2*math.pi*n/N)
+    return (x*math.cos(rotation) + y*math.sin(rotation), -x*math.sin(rotation) + y*math.cos(rotation))
 
 def midpoint(x1, y1, x2, y2):
-    return ((x2 + x1) / 2, (y2 + y1 / 2))
+    return ((x1 + x2) / 2.0, (y1 + y2) / 2.0)
 
-r = random.randint(1,num)
+if shape == 't':
+    N = 3
+    a, b, c = pointfinder(N,0), pointfinder(N,1), pointfinder(N,2)
+
+if shape == 's':
+    N = 4
+    a, b, c, d = pointfinder(N,0), pointfinder(N,1), pointfinder(N,2), pointfinder(N,3)
+
+if shape == 'p':
+    # http://mathworld.wolfram.com/RegularPentagon.html
+    N = 5
+    a, b, c, d, e = pointfinder(N, 0), pointfinder(N, 1), pointfinder(N, 2), pointfinder(N, 3), pointfinder(N, 4)
+
+r = random.randint(1,N)
 
 if r == 1:
     # A is chosen as first point
-    r = random.randint(1,num - 1)
+    r = random.randint(1,N - 1)
     if r == 1:
         x = midpoint(a[0], a[1], b[0], b[1])
     if r == 2:
@@ -61,7 +60,7 @@ if r == 1:
         x = midpoint(a[0], a[1], e[0], e[1])
 if r == 2:
     # B is chosen as the second pt
-    r = random.randint(1,num - 1)
+    r = random.randint(1,N - 1)
     if r == 1:
         x = midpoint(b[0], b[1], a[0], a[1])
     if r == 2:
@@ -72,7 +71,7 @@ if r == 2:
         x = midpoint(b[0], b[1], e[0], e[1])
 if r == 3:
     #C is chosen as the third pt
-    r = random.randint(1,num - 1)
+    r = random.randint(1,N - 1)
     if r == 1:
         x = midpoint(c[0], c[1], a[0], a[1])
     if r == 2:
@@ -82,8 +81,8 @@ if r == 3:
     if r == 4:
         x = midpoint(c[0], c[1], e[0], e[1])
 if r == 4:
-    #C is chosen as the fourth pt
-    r = random.randint(1,num - 1)
+    #D is chosen as the fourth pt
+    r = random.randint(1,N - 1)
     if r == 1:
         x = midpoint(d[0], d[1], a[0], a[1])
     if r == 2:
@@ -91,10 +90,10 @@ if r == 4:
     if r == 3:
         x = midpoint(d[0], d[1], c[0], c[1])
     if r == 4:
-        x = midpoint(d[0], d[1], d[0], d[1])
+        x = midpoint(d[0], d[1], e[0], e[1])
 if r == 5:
-    #C is chosen as the fifth pt
-    r = random.randint(1,num - 1)
+    #E is chosen as the fifth pt
+    r = random.randint(1,N - 1)
     if r == 1:
         x = midpoint(e[0], e[1], a[0], a[1])
     if r == 2:
@@ -107,25 +106,24 @@ if r == 5:
 #xvals.append(x[0])
 #yvals.append(x[1])
 
-while i < N:
-    r = random.randint(1,num)
+while i < numiter:
+    r = random.randint(1,N)
     if r == 1: #A
-        x = midpoint(x[0],x[1],a[0],a[1])
+        x = midpoint(x[0], x[1], a[0], a[1])
     if r == 2: #B
         x = midpoint(x[0], x[1], b[0], b[1])
     if r == 3: #C
         x = midpoint(x[0], x[1], c[0], c[1])
     if r == 4: #D
         x = midpoint(x[0], x[1], d[0], d[1])
-    if r == 5: #C
+    if r == 5: #E
         x = midpoint(x[0], x[1], e[0], e[1])
     xvals.append(x[0])
     yvals.append(x[1])
     i += 1
 
-plt.scatter(xvals, yvals,s = 0.5, c = "r", marker = "o")
+plt.scatter(xvals, yvals, s = 0.5, c = "r", marker = "o")
 plt.title("Chaos Game")
 plt.axis('off')
-plt.tight_layout()
-
+#plt.tight_layout()
 plt.show(block = True)
